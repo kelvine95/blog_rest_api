15d94d326de343c62050c0f2f2d69b56a49eae75
ighodaro <ighodaro@bitbucket.org>
2020-10-18 17:17:45 +0100

    created objects in the models.py file for both the organizer and post

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from organizer.models import Tag, Startup
+
+class Post(models.Model):
+    """Blog post; news article about startups"""
+    title = models.CharField(max_length = 63)
+    slug = models.SlugField(max_length=63)
+    text = models.TextField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
+
+    class Meta:
+        get_latest_by = "pub_date"
+        ordering = ["- pub_date", "title"]
+        verbose_name = "blog post"
+
+    def __str__(self):
+        date_string = self.pub_date.strftime("%Y-%m-%d")
+        return f"{self.title} on {date_string}"
+
